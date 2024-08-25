c7f58aeb47b1320243fdfc9ed175bcfe8dff3876
ANUSHKA78613 <anushkalodhi785@gmail.com>
2024-08-25 13:03:02 +0530

    Add files via upload

---
+++ vari.kt
@@ -0,0 +1,54 @@
+
+fun main(args:Array<String>) {
+    var a=40
+    var b=30
+    val s=a+b
+    println(s)
+    val bb=23
+    val cc=11
+    var ss=bb+cc
+    print(ss)
+
+
+    var a1:Int
+    a1=10
+    val b1=10
+    var c:Int
+    c=a1+b1
+    print("---"+c+"----")
+
+
+
+
+    var age: Int=10
+    println(age) }
+
+
+    val message: String="Hello, Kotlin!"
+    println(message)
+
+
+
+
+    val a2=10
+    val b2=20
+    val result="sum of $a2 and $b2 is ${a2+b2}"
+    println(result)
+
+
+
+    var v="This is single line string"
+    println(v)
+    println('V')
+    print('\$')
+    print('\\')
+    var y:String=""" This is multiple
+    line string"""
+    println(y)
+    var mm=""" thksudhfkya
+    dghkudsg"""
+    print(mm)
+    val w:Boolean=true
+    print(w)
+
+}
