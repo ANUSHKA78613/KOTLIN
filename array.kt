b0965a1ac1bc37fd8af17453c183b3ddcb41d77b
ANUSHKA78613 <anushkalodhi785@gmail.com>
2024-08-25 13:03:37 +0530

    Add files via upload

---
+++ array.kt
@@ -0,0 +1,17 @@
+fun main(args:Array<String>){
+    //	valnumbers=arrayOf(1,2,3,4,5)
+    //al words:Array<String>=arrayOf("kotlin","java")
+    //rint(words[0])
+    //val squares=Array[5]{
+    //  i->i*i}
+    //rint(squares[0])
+    val numbers=arrayOf(1,2,3,4,5)
+    print(numbers.size)
+    if(numbers.isEmpty())
+    {
+        println("empty")
+    }
+
+}
+
+
